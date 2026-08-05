de8969a4646fff2dbf574bda92666be9f8c8fa2d
mld8nkRARE <mld8nk@tutamail.com>
2026-08-05 15:19:22 +0300

    feat(frontend): подключил vuetify, добавил навигационную панель

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <router-view />
-  </div>
+   <v-app>
+    <v-app-bar>
+      <v-btn to="/">Статьи</v-btn>
+      <v-btn to="/analytic">Аналитика</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
